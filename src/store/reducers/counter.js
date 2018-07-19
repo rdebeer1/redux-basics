891bbd232e15a464fa86684ff82f4e230b4f5b72
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-07-19 18:21:13 -0500

    implements basic middleware for logging

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -26,8 +26,9 @@ const reducer = (state = initialState, action) => {
         ...state,
         counter: state.counter - action.val
       }
+    default:
+      return state;
   }
-  return state;
 }
 
 export default reducer;
