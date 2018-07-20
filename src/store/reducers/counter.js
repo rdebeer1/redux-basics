fa0e9003e086cbb041fd044e95cd676efe2b1330
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-07-19 20:35:35 -0500

    leans reducer switch statements

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -5,19 +5,31 @@ const initialState = {
   counter: 0,
 }
 
+const increment = (state, action) => {
+  return updateObject(state, { counter: state.counter + 1 });
+}
+
+const decrement = (state, action) => {
+  return updateObject(state, { counter: state.counter - 1 });
+}
+
+const add = (state, action) => {
+  return updateObject(state, { counter: state.counter + action.val });
+}
+
+const subtract = (state, action) => {
+  return updateObject(state, { counter: state.counter - action.val });
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.INCREMENT:
-      return updateObject(state, { counter: state.counter + 1 });
-    case actionTypes.DECREMENT:
-      return updateObject(state, { counter: state.counter - 1 });
-    case actionTypes.ADD:
-      return updateObject(state, { counter: state.counter + action.val });
-    case actionTypes.SUBTRACT:
-      return updateObject(state, { counter: state.counter - action.val });
+    case actionTypes.INCREMENT: return increment(state, action);
+    case actionTypes.DECREMENT: return decrement(state, action);
+    case actionTypes.ADD: return add(state, action);
+    case actionTypes.SUBTRACT: return subtract(state, action);
     default:
       return state;
   }
-}
+};
 
 export default reducer;
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,16 +5,21 @@ const initialState = {
   results: []
 }
 
+const storeResult = (state, action) => {
+  return updateObject(state,{ results: state.results.concat({ id: new Date(), value: action.result })});
+}
+
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+  return updateObject(state, {results: updatedArray });
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.STORE_RESULT:
-      return updateObject(state,{ results: state.results.concat({ id: new Date(), value: action.result })});
-    case actionTypes.DELETE_RESULT:
-      const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-      return updateObject(state, {results: updatedArray });
-    default:
-      return state;
+    case actionTypes.STORE_RESULT: return storeResult(state, action);
+    case actionTypes.DELETE_RESULT: return deleteResult(state, action);
+    default: return state;
   }
-}
+};
 
 export default reducer;
